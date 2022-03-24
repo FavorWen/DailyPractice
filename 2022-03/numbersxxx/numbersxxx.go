cf2fd248cc3cf7fcf2019a3dbf77aca89dca80ac
FavorWen <1597230820@qq.com>
2022-03-24 11:20:03 +0800

    numbersxxx 120min pass

--- 2022-03/numbersxxx/numbersxxx.go
+++ 2022-03/numbersxxx/numbersxxx.go
@@ -4,66 +4,72 @@ import (
 	"fmt"
 )
 
-func upground(n int) (int, int, int) {
-	r := 0
+func upground(n int) int {
 	k := 9
 	sum := 0
 	bit := 0
 	for k >= 0 && sum < n {
-		r = r*10 + k
 		sum += k
 		k--
 		bit++
 	}
 	if k < 0 && sum < n {
-		return -1, bit, 0
+		return -1
 	}
-	return r, bit, sum
+	return bit
 }
 
-func downground(up, bits, sum int) int {
-	r := 0
-	k := 1
-	for ; bits > 0; bits-- {
-		r = r*10 + k
-		k++
+func sum(nums []int, n int) (int, int) {
+	if len(nums) < n {
+		return -1, -1
 	}
-	return r
+	d := 0
+	for i := 0; i < n; i++ {
+		d += nums[i]
+	}
+	u := 0
+	for i := len(nums) - 1; i > len(nums)-n-1; i-- {
+		u += nums[i]
+	}
+	return d, u
+}
+
+func find(target int, nums []int, n int) []int {
+	if target == 0 {
+		return []int{}
+	}
+	if target != 0 && len(nums) < n {
+		return nil
+	}
+	down, up := sum(nums, n)
+	if !(target >= down && target <= up) {
+		return nil
+	}
+	for i := range nums {
+		r := find(target-nums[i], nums[i+1:], n-1)
+		if r != nil {
+			return append([]int{nums[i]}, r...)
+		}
+	}
+	return nil
 }
 
 func solve(n int) int {
-	up, bits, sum := upground(n)
-	if sum == n {
-		return up
+	bits := upground(n)
+	//fmt.Println(bits)
+	if bits == -1 {
+		return -1
 	}
-	down := downground(up, bits, sum)
-	//fmt.Println(up, down, bits)
-	for i := down; i <= up; i++ {
-		sum := 0
-		pool := make(map[int]int)
-		flag := false
-		k := i
-		for ; k != 0; k = k / 10 {
-			j := k % 10
-			if _, ok := pool[j]; ok {
-				flag = true
-				break
-			}
-			sum += j
-			if sum > n {
-				flag = true
-				break
-			}
-			pool[j] = 0
-		}
-		if flag {
-			continue
-		}
-		if sum == n {
-			return i
-		}
+	nums := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	r := find(n, nums, bits)
+	if r == nil {
+		return -1
 	}
-	return -1
+	ans := 0
+	for i := range r {
+		ans = ans*10 + r[i]
+	}
+	return ans
 }
 
 func main() {
